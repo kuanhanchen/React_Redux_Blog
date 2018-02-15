36a0bfa082a46c7a77fa462a65c4adc9286c4286
Kuan-Han Chen <kuanhanchen0608@gmail.com>
2018-02-15 14:09:31 -0800

    Navigation with the Link Component

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route  } from 'react-router-dom';
+import { BrowserRouter, Route, Switch  } from 'react-router-dom';
 import reducers from './reducers';
 
 import promise from 'redux-promise';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <div>
-        <Route path="/" component={PostsIndex} />
-      </div> 
+      	<Switch>
+			<Route path="/posts/new" component={PostsNew} />
+			<Route path="/" component={PostsIndex} />
+		</Switch>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
