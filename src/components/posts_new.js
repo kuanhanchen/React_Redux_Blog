389b1f9b33270c69b7b64ac830d872d94668938c
Kuan-Han Chen <kuanhanchen0608@gmail.com>
2018-02-16 17:08:55 -0800

    Showed form submit result in console

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,71 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-	render() {
+	renderField(field) {
 		return (
-			<div>
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input 
+					className="form-control"
+					type="text"
+					{...field.input}
+				/>
+				{field.meta.error}
 			</div>
 		);
 	}
+
+	onSubmit(values) {
+		console.log(values);
+	}
+
+	render() {
+		const { handleSubmit } = this.props;
+		
+		return (
+			<div>
+				<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+					<Field 
+						label="Title"
+						name="title"
+						component={this.renderField}
+					/>
+					<Field 
+						label="Categories"
+						name="categories"
+						component={this.renderField}
+					/>
+					<Field 
+						label="Post Content"
+						name="content"
+						component={this.renderField}
+					/>
+					<button type="submit" className="btn btn-primary">Submit</button>
+				</form>
+			</div>
+		);
+	}
+
 }
 
-export default PostsNew;
+function validate(values) {
+	const errors = {};
+
+	if (!values.title) {
+		errors.title = "Enter a title!";
+	}
+	if (!values.categories) {
+		errors.categories = "Enter some categories!";
+	}
+	if (!values.content) {
+		errors.content = "Enter some content please!";
+	}
+
+	return errors;
+}
+
+export default reduxForm({
+	validate,
+	form: 'PostsNewForm'
+})(PostsNew);
