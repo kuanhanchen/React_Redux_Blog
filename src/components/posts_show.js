702b7f9a685170788153b194b50ae46b702a8249
Kuan-Han Chen <kuanhanchen0608@gmail.com>
2018-02-17 12:18:47 -0800

    Added back to index button in post detail page

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
 
@@ -19,6 +20,9 @@ class PostsShow extends Component {
 		
 		return (
 			<div>
+				<div className="text-xs-right">
+					<Link to="/" className="btn btn-primary">Back To Index</Link>
+				</div>
 				<h3>{ post.title }</h3>
 				<h6>Categories: {post.categories}</h6>
 				<p>{post.content}</p>
